4b676dd359862245b1346fcea0ca0a1e55357bc0
Dorien van Blooijs <d.vanblooijs@umcutrecht.nl>
2020-09-23 18:45:23 +0200

    fixed false,true in json file instead of 0,1

--- trc2bids/functions/write_json.m
+++ trc2bids/functions/write_json.m
@@ -2,7 +2,9 @@ function write_json(filename, json)
 json = remove_empty(json);
 ft_info('writing %s\n', filename);
 if ft_hastoolbox('jsonlab', 3)
-    savejson('', json, filename);
+    opt.ParseLogical = 1; % logical array elem will use true/false rather than 1/0.
+    opt.FileName = filename;
+    savejson('', json, opt);
 else
     str = jsonencode(json);
     fid = fopen(filename, 'w');
