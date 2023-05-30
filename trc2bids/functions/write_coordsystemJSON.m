009dfdb105e35fc3f86d257a489e1c5a662d3cf0
Dorien van Blooijs <d.vanblooijs@umcutrecht.nl>
2023-05-30 15:26:39 +0200

    updated coordsystem to write after localizing electrodes

--- trc2bids/functions/write_coordsystemJSON.m
+++ trc2bids/functions/write_coordsystemJSON.m
@@ -5,63 +5,44 @@ function write_coordsystemJSON(cfg)
 %%%% This can be found here: https://github.com/gllmflndn/JSONio
 
 %%%% Dora Hermes, Jaap van der Aar, Giulio Castegnaro, Dorien van Blooijs 2019
-for ii=1:size(cfg(1).ses_dir,2)
+%%
 
-    sub_temp = extractBetween(cfg(1).ses_dir{ii},'/sub','/ses');
-    sub_label = ['sub', sub_temp{1}];
-    ses_label = ['ses', extractAfter(cfg(1).ses_dir{ii},'/ses')];
+coord_json_name = fullfile(cfg(1).ieeg_directory,...
+    [cfg(1).sub_labels{:} '_' cfg(1).ses_label '_coordsystem.json']);
 
-    coord_json_name = fullfile(cfg(1).ses_dir{ii},'ieeg',...
-        [sub_label '_' ses_label '_coordsystem.json']);
+% This line is to create a variable for the name of the file intendedFor
+filename_T1w = fullfile(cfg(1).sub_labels{:},cfg(1).ses_label, 'anat', [cfg(1).sub_labels{:} '_' cfg(1).ses_label '_rec-deface_T1w.nii']);
 
-    % This line is to create a variable for the name of the file intendedFor
-    filename_T1w = fullfile(sub_label,ses_label, 'anat', [sub_label '_' ses_label '_rec-deface_T1w.nii']);
+if exist(fullfile(cfg(1).proj_diroutput,filename_T1w),'file')
+    % assign information and methodology
+    loc_json.IntendedFor = filename_T1w;
+    loc_json.iEEGCoordinateSystem  = 'Other';
+    loc_json.iEEGCoordinateUnits  = 'mm';
+    loc_json.iEEGCoordinateSystemDescription = 'The origin of the coordinate system is between the ears and the axis are in the RAS direction. The scaling is with respect to the individuals anatomical scan and no scaling or deformation have been applied to the individuals anatomical scan';
+    loc_json.iEEGCoordinateProcessingDescription = 'Surface projection Hermes or Branco';
+    loc_json.iEEGCoordinateProcessingReference = 'Hermes et al., 2010 JNeuroMeth , Branco et al., 2018 JNeuroMeth';
 
-    if exist(fullfile(cfg(ii).proj_diroutput,filename_T1w),'file')
-        % assign information and methodology
-        loc_json.IntendedFor = filename_T1w;
-        loc_json.iEEGCoordinateSystem  = 'Other';
-        loc_json.iEEGCoordinateUnits  = 'mm';
-        loc_json.iEEGCoordinateSystemDescription = 'The origin of the coordinate system is between the ears and the axis are in the RAS direction. The scaling is with respect to the individuals anatomical scan and no scaling or deformation have been applied to the individuals anatomical scan';
-        loc_json.iEEGCoordinateProcessingDescription = 'Surface projection Hermes or Branco';
-        loc_json.iEEGCoordinateProcessingReference = 'Hermes et al., 2010 JNeuroMeth , Branco et al., 2018 JNeuroMeth';
+    jsonSaveDir = fileparts(coord_json_name);
 
-        jsonSaveDir = fileparts(coord_json_name);
-
-        % ensure there is a /ieeg/ folder
-        if ~isfolder(jsonSaveDir)
-            fprintf('Warning: directory to save json file does not exist, create: %s \n',jsonSaveDir)
-        end
-
-        % json_options.indent = '    '; % this just makes the json file look prettier
-        %
-        % % write JSON file
-        % jsonwrite(electrodes_json_name,loc_json,json_options)
-
-        write_json(coord_json_name, loc_json)
+    % ensure there is a /ieeg/ folder
+    if ~isfolder(jsonSaveDir)
+        fprintf('Warning: directory to save json file does not exist, create: %s \n',jsonSaveDir)
     end
+
+    % % write JSON file
+    if ~isempty(loc_json)
+
+        delete(coord_json_name)
+        write_json(coord_json_name, loc_json)
+        fileattrib(coord_json_name,'-w -x','o') % make not-writable and not-executable for other users
+        fileattrib(coord_json_name,'+w +x','g') % make writable and executable (required for folders to open them) for group users
+
+    end
+
 end
 end
 
 
-function write_json(filename, json)
-json = remove_empty(json);
-ft_info('writing %s\n', filename);
-if ft_hastoolbox('jsonlab', 3)
-    savejson('', json, filename);
-else
-    str = jsonencode(json);
-    fid = fopen(filename, 'w');
-    fwrite(fid, str);
-    fclose(fid);
-end
-end
-
-function s = remove_empty(s)
-fn = fieldnames(s);
-fn = fn(structfun(@isempty, s));
-s = removefields(s, fn);
-end
 
 
 
